d9b8437d2561240ab743777f2a1f09b5ecf45671
Anastasiia <troyanovskaya2003@gmail.com>
2024-05-29 23:00:29 +0100

    author

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -37,5 +37,6 @@ const deleteReview = async(req, res) =>{
 
 module.exports = { 
     postReview,
-    getReviewByBookId
+    getReviewByBookId,
+    deleteReview
 }
--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const reviewRouter = express.Router();
 
-const { postReview, getReviewByBookId} = require('../controllers/reviewController')
+const { postReview, getReviewByBookId, deleteReview} = require('../controllers/reviewController')
 
 reviewRouter.route('/').post(postReview);
+reviewRouter.route('/:id').delete(deleteReview);
 reviewRouter.route('/books/:bookId').get(getReviewByBookId);
 
 module.exports = reviewRouter;
